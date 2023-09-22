ce6f461dcce15e06d4b5fcc57fec9776c4455785
n-stepanova <nstepanovaqa@gmail.com>
2023-09-22 20:48:58 +0300

    hw13_1

---
+++ src/main/java/HW13_1.java
@@ -0,0 +1,24 @@
+public class HW13_1 {
+//    Необходимо написать 4 метода:
+//    сложение 2х чисел
+//    вычитание 2х чисел
+//    умножение 2х чисел
+//    деление 2х чисел
+//    Для каждого метода нужно написать тест используя TestNG.
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int division(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/test/java/HW131Test.java
@@ -0,0 +1,29 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HW131Test {
+
+    @Test
+    public void testSum(){
+        int sum = HW13_1.sum(-5, 5);
+        Assert.assertEquals(sum, 0);
+    }
+
+    @Test
+    public void testSubtraction(){
+        int sub = HW13_1.subtraction(-5, 5);
+        Assert.assertEquals(sub, -10);
+    }
+
+    @Test
+    public void testMultiply(){
+        int multiply = HW13_1.multiply(-5, 5);
+        Assert.assertEquals(multiply, -25);
+    }
+
+    @Test
+    public void testDivision(){
+        int division = HW13_1.division(-5, 5);
+        Assert.assertEquals(division, -1);
+    }
+}
